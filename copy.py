67eb9a9fb81b93b335bd64a233c9721025b6cd43
jesuscovam <46403187+jesuscovam@users.noreply.github.com>
2019-05-22 17:22:33 -0500

    copy.py

---
+++ copy.py
@@ -0,0 +1,16 @@
+import pyperclip
+
+def uno(variable):
+    producto = variable
+    producto = producto.lower()
+    lista = producto.split(" ")
+    lista2= []
+    
+    for x in lista:
+             lista2.append(x.capitalize())
+
+    dos = ' '.join(lista2)
+    pyperclip.copy(dos)
+
+    
+uno('HOLA COMO ESTAS')
